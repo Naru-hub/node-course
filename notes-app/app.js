b385ae5418220834349ddaed3c42e4bbfafa453d
Naru-hub <1208ny0324@gmail.com>
2022-08-12 21:38:00 +0900

    Global npm Modules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,22 +1,8 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
-const log = console.log;
 
 const msg = getNotes();
 console.log(msg);
-log(chalk.green('Success!'));
-log(chalk.bold.green.inverse('Success!'));
-log(chalk.cyan('Hello!'));
-log(chalk.bgBlue.bold('Success2!'));
 
-//
-// Challenge: Use the chalk library in your project
-//
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-//
-// Bonus: Use docs to mess around with other styles. Make text hold and inversed.
-
-
+const greenMsg = chalk.blue.inverse.bold('Success!');
+console.log(greenMsg);
